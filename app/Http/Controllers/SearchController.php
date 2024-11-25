3abc4b7363f9f9c45a56829cd7cce0c042673fb8
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-25 22:18:14 +0900

    コントローラー(search)変更

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    //検索画面のTop
+    public function searchTop()
+    {
+        return view('search.searchTop');
+    }
+
+    //作者のあいまい検索の画面
+    public function searchWritter()
+    {
+        return view('search.searchWriter');
+    }
+
+    //書籍名のあいまい検索の画面
+    public function seartchTitle()
+    {
+        return view('search.searchTitle');
+    }
+
+    //あいまい検索の処理
+    public function searchList(Request $req)
+    {
+        $word = $req->input('word'); //入力したキーワードを取得
+        $type = $req->input('type'); //作者名か書籍名か
+
+        if ($type === 'writer') {
+            $results = Book::where('writer', 'LIKE', '%{writer}%')->get();
+        } elseif ($type === 'title') {
+            $results = Book::where('title', 'LIKE', '%{title}%')->get();
+        }
+
+        //ビュー(searchList)に結果の連想配列を送る
+        return view('search.searchList', ['reuslts' => $results, 'word' => $word, 'type' => $type]);
+    }
+
+
+
+    // 書籍詳細ページを表示
+    public function show($id)
+    {
+        // 指定したIDの書籍を取得
+        $book = Book::findOrFail($id);
+
+        // 詳細ビューに書籍データを渡す
+        return view('books.show', ['book' => $book]);
+    }
+}
