da3c937650664b981e9116d8fa89755d42f5361c
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-26 03:07:42 +0900

    bookbblade変更

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>書籍詳細</title>
 </head>
+
 <body>
     <h1>書籍詳細情報</h1>
     <a href="/">トップページに戻る</a><br>
@@ -14,6 +16,15 @@
         <p>{{$book->writer}}</p>
         <p>価格: ¥{{ $book->price }}</p>
         <p>ISBN: {{ $book->isbn }}</p>
+
+        <h2>レビュー</h2>
+        @if($reviews->isNotEmpty())
+        @foreach($reviews as $review)
+        <p>レビュー: {{ $review->content }}</p>
+        @endforeach
+        @else
+        <p>レビューはまだありません。</p>
+        @endif
     </div>
     <!-- <form action="/" method="post">
         @csrf
@@ -31,4 +42,5 @@
         <input type="submit" value="削除" name="delete_review">
     </form> -->
 </body>
+
 </html>
