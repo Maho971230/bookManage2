edcc805e2b4b6b603ef86a955ed9b5e5129d4a09
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-26 03:50:31 +0900

    コントローラー編集

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\Employee;
+use App\Models\Review;
 
 class ReviewController extends Controller
 {
-    public function 
+    //新規レビュー登録画面
+    public function reviewCreate()
+    {
+        return view('review.reviewCreate');
+    }
+
+    //新規登録確認画面
+    public function postconf(Request $req) {}
+
+    //新規登録完了画面
+    public function reviewStore(Request $req) {}
+
+    //レビュー編集画面
+    public function edit()
+    {
+        return view('review.edit');
+    }
+
+    //レビュー編集確認画面
+    public function repost
 }
