6f0893a7645662e49c728d7f8f8076d7927795a3
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-25 22:37:14 +0900

    ルート(search)を変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップページ</title>
 </head>
+
 <body>
     <h1>書籍管理システム</h1>
     <!-- <form action="/login" method="post"><input type="submit" value="ログアウト"></form> -->
     <ul>
         <li><a href="/create">書籍登録</a></li>
-        <li><a href="/input">書籍検索</a></li>
+        <li><a href="/searchTop">書籍検索</a></li>
         <li><a href="/list">書籍一覧</a></li>
     </ul>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,19 @@ Route::get('/', function () {
     return view('top');
 });
 
-// Route::post('/',[])
+// 
 Route::get('/create', [RegController::class, 'create']);
 Route::post('/check', [RegController::class, 'create']);
 Route::post('/store', [RegController::class, 'store']);
 
+//一覧を表示
 Route::get('/list', [RegController::class, 'list']);
 
+//検索Topを表示
+Route::get('/searchTop', [SearchController::class, 'searchTop'])->name('search.searchTop');
+//作者から検索を表示
+Route::get('/searchWriter', [SearchController::class, 'searchWriter'])->name('search.searchWriter');
+//書籍から検索を表示
+Route::get('/searchTitle', [SearchController::class, 'searchTitle'])->name('search.searchTitle');
+//検索結果を表示
+Route::post('/searchList', [SearchController::class, 'searchList'])->name('search.searchList');
