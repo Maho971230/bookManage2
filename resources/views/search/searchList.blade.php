1aec2d4e49d6e8265923de3068af6e8b277df130
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-25 23:36:46 +0900

    searchList変更

--- resources/views/search/searchList.blade.php
+++ resources/views/search/searchList.blade.php
@@ -1,20 +1,25 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>検索結果</title>
     {{-- BootStrapから引用 --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 
 </head>
+
 <body>
     <a href="/">Topページに戻る</a>
     <h1>検索結果</h1>
+    @if($results->isEmpty())
+    <p>該当する書籍はありません。</p>
+    @else
     <table class="table">
         <thead>
             {{-- 目次 --}}
@@ -29,12 +34,13 @@
             {{-- 中身 --}}
             <tr>
                 <th scope="row">1</th>
-                    <td><a href="/book">{{$result->title}}</a></td>
-                    <td>{{$result -> writer}}</td>
+                <td><a href="/book">{{$result->title}}</a></td>
+                <td>{{$result -> writer}}</td>
             </tr>
             @endforeach
         </tbody>
-
     </table>
+    @endif
 </body>
+
 </html>
