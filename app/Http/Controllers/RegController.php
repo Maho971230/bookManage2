5ca9590dd1b3f97f4fdc6c88a17348b8f0dfb5a8
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-25 16:52:29 +0900

    コントローラー変更

---
+++ app/Http/Controllers/RegController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Book;
+
+class RegController extends Controller
+{
+    public function create() {}
+
+    public function store() {}
+
+    public function list()
+    {
+        $data = [
+            'records' => Book::all()
+        ];
+        return view('list', $data);
+    }
+}
