8516a8b6d3a472846c1aa34ad5b3ab723de590db
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-26 16:15:50 +0900

    変更

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
 
-    protected $fillable = ['content', 'rating'];
-    
+    protected $fillable = [
+        'content',
+        'rating',
+        'employee_id'
+    ];
+
     public function book()
     {
         return $this->belongsTo(Book::class);
