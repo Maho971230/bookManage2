327229d82269a11108029c0044ce0b0905fa1cc9
Kotaro Shinjo <ks.joe1883@gmail.com>
2024-11-26 13:51:57 +0900

    変更

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
+
+    protected $fillable = [
+        'content',
+        'rating'
+    ];
+
     public function reviews()
     {
         return $this->hasMany(Review::class);
