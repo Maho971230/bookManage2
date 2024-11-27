e7ca4b4e49496fbae01672249976659cddc0d9e8
ren <kozasa14741@gmail.com>
2024-11-27 15:03:07 +0900

    11/27(小笹変更)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ログイン画面</title>
+        <!-- 外部CSSを読み込む -->
+        <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <h1>社員名とパスワードを入力してください</h1>
