4b3eaabf9078a25a6a1c4e23943e12bef1a5d640
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 10:35:14 +0900

    Update book.blade.php

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -4,24 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>書籍詳細</title>
 </head>
 <body>
     <h1>書籍詳細情報</h1>
     
-    <form action="" method="">
+    <form action="/" method="post">
         @csrf
         書籍名{{}}
-        <img src="" alt="書籍の画像" width="" height="">
+        <img src="/" alt="書籍の画像" width="" height="">
         著書名{{}}
         出版社名{{}}
         価格{{}}
         ISBN{{}}
-        レビュー{{}}
+        レビュー{{}} <input type="submit" value="投稿" name="post_review">
         評価{{}}
         口コミ{{}}
+    
+        <input type="submit" value="編集" name="update_review">
+        <input type="submit" value="削除" name="delete_review">
     </form>
-    <input type="submit" value="編集">
-    <input type="sumbit" value="削除">
 </body>
 </html>
