6d48ec5059555addeefef485d7899536b89de2e1
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 15:51:22 +0900

    Update searchWriter.blade.php

--- resources/views/search/searchWriter.blade.php
+++ resources/views/search/searchWriter.blade.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>書籍検索</title>
+    <title>著者名検索</title>
 </head>
 <body>
-    <h1>書籍名(キーワード)を入力してください</h1>
-    <form action="/search/searchTitle" method="post"><a href="/">トップページに戻る</a>
-        <input type="text" name="title" required><input type="submit" value="検索">
+    <h1>著者名(キーワード)を入力してください</h1>
+    <form action="/searchList" method="post"><a href="/">トップページに戻る</a>
+        @csrf
+        <input type="hidden" name="type" value="writer">
+        <input type="text" name="word" required><input type="submit" value="検索">
     </form>
 </body>
 </html>
