f55bb6ca428e9c99da7eafa5f856e5744840c3bf
ren <kozasa14741@gmail.com>
2024-11-27 09:47:46 +0900

    11/27（小笹）

---
+++ resources/views/review/search/searchlist.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>検索結果</title>
+    {{-- bootstrap読み込み --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</head>
+<body>
+    <h1>検索結果</h1>
+    <a href="">トップページに戻る</a>
+    <table class="table">
+        <thead>
+            <th scope="col">番号</th>
+            <th scope="col">書籍名</th>
+            <th scope="col">著書名</th>
+        </thead>
+        <tbody>
+            {{-- 1番から表示したい書籍名・著書名を入力する必要あり --}}
+            <tr>
+                <th scope="row">1</th>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
