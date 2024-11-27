c5db7048d95a545d1ea4a32f1d29b3c9a8daa843
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 15:56:52 +0900

    Update check.blade.php

--- resources/views/reg/check.blade.php
+++ resources/views/reg/check.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,10 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
-
 <body>
     <h1>この内容で登録してもよろしいですか？</h1>
-
     <form action="/store" method="post">
         @csrf
         <input type="hidden" name="id" value={{$data->id}}><br>
@@ -20,7 +17,7 @@
             <tr>
                 <th>{{$data->isbn}}</th>
                 <th>{{$data->title}}</th>
-                <th>{{$data->author}}</th>
+                <th>{{$data->writer}}</th>
                 <th>{{$data->publisher}}</th>
                 <th>{{$data->price}}</th>
             </tr>  
@@ -29,3 +26,4 @@
         <a href="/create">前のページに戻る</a>
      </form>
 </body>
+</html>
