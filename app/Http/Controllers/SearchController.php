2ef508c7e34dc6ec34ad06d4cc503ffec66a351e
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 14:18:03 +0900

    Update SearchController.php

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
+use App\Models\Employee;
+use App\Models\Review;
 
 class SearchController extends Controller
 {
