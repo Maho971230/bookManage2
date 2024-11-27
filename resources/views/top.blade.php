0193c5a63c4f9bf5c381883ba6626f8ec28a2ada
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 15:57:39 +0900

    Update top.blade.php

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>トップページ</title>
 </head>
-
 <body>
     <h1>書籍管理システム</h1>
     <!-- <form action="/login" method="post"><input type="submit" value="ログアウト"></form> -->
@@ -16,5 +14,4 @@
         <li><a href="/list">書籍一覧</a></li>
     </ul>
 </body>
-
 </html>
