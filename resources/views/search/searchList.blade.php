9756b63a1b5c9a1a1e60dd7c17e26b50848df675
ren <kozasa14741@gmail.com>
2024-11-27 09:57:10 +0900

    11/27（小笹）

---
+++ resources/views/search/searchList.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>検索結果</title>
+    {{-- BootStrapから引用 --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <a href="">Topページに戻る</a>
+    <h1>検索結果</h1>
+    <table class="table">
+        <thead>
+            {{-- 目次 --}}
+            <tr>
+                <th scope="col">番号</th>
+                <th scope="col">書籍名</th>
+                <th scope="col">著書名</th>
+            </tr>
+        </thead>
+        <tbody>
+            {{-- 中身 --}}
+            <tr>
+                <th scope="row">1</th>
+                    <td></td>
+                    <td></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
