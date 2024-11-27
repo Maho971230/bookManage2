a5102578060ee853cce9474b6ad833c9b6a483fd
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-27 11:37:10 +0900

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
