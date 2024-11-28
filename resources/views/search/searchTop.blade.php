74119b88d2163e7c0cb27fed5894fcc7ad1e0fa8
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-28 11:46:15 +0900

    Update searchTop.blade.php

--- resources/views/search/searchTop.blade.php
+++ resources/views/search/searchTop.blade.php
@@ -6,7 +6,7 @@
     <title>書籍検索</title>
 </head>
 <body>
-    <h1>書籍検索</h1><a href="/">トップページに戻る</a>
+    <h1>書籍検索</h1><a href="/top">トップページに戻る</a>
     <a href="/searchTitle">書籍名で検索</a>
     <a href="/searchWriter">著者名で検索</a>
 </body>
