df1d60fbee4ca6d0b038761bc6fbba0fb43d71aa
joe1883 <ks.joe1883@gmail.com>
2024-11-28 20:31:35 +0900

    Update store.blade.php

--- resources/views/reg/store.blade.php
+++ resources/views/reg/store.blade.php
@@ -6,15 +6,17 @@
     <title>登録完了画面</title>
 </head>
 <body>
-    <h1>書籍登録完了</h1>
-    <h2>以下の内容で登録しました</h2>
-    <br>
-    <p>ISBN     {{}}</p>
-    <p>書籍名   {{}}</p>
-    <p>著者名   {{}}</p>
-    <p>出版社名 {{}}</p>
-    <p>価格     {{}}</p>
-    <br>
-    <a href="/top">トップページに戻る</a>
+    <div class="container mt-5">
+        <h1>登録完了</h1>
+        <p>以下の書籍が登録されました:</p>
+        <ul>
+            <li>ISBN: {{ $data['isbn'] }}</li>
+            <li>書籍名: {{ $data['title'] }}</li>
+            <li>著者名: {{ $data['writer'] }}</li>
+            <li>出版社: {{ $data['publisher'] }}</li>
+            <li>価格: {{ $data['price'] }}</li>
+        </ul>
+        <a href="{{ route('create') }}">新しい書籍を登録</a>
+    </div>
 </body>
 </html>
