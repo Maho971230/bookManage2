09fc2be65a3ee1ab1d3b68449f43f4f6afcf051f
joe1883 <ks.joe1883@gmail.com>
2024-11-28 18:10:41 +0900

    Update erase.blade.php

--- resources/views/review/erase.blade.php
+++ resources/views/review/erase.blade.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>レビュー削除画面</title>
-</head>
-<body>
+  </head>
+  <body>
     <h1>このレビューを削除しますか？</h1>
-    <form action="" method="">
-        @csrf
-        <h1>確認内容</h1>
-        書籍名{{}}
-        著書名{{}}
-        レビュー内容{{}}
+    <form action="{{ route('delete') }}" method="post">
+      @csrf
+      <h1>確認内容</h1>
+      <input type="hidden" name="id" value="{{$value-" />id}}>
+      <br />
+      <p>書籍名:{{ $review->book->title }}</p>
+      <br />
+      レビュー内容:
+      <p>{{ old('content', $review->content) }}</p>
+      <br />
+      評価:
+      <p>{{ old('rating', $review->rating) }}</p>
+      <br />
+      <input type="submit" value="削除" class="btn btn-primary" />
     </form>
-    <input type="submit" value="削除">
-    <a href="">戻る</a>
-</body>
+    <a href="{{ route('top') }}">戻る</a>
+  </body>
 </html>
