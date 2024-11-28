a54bb0ab3777024ecdbfac6b0b8d6904294b6149
joe1883 <ks.joe1883@gmail.com>
2024-11-28 16:32:05 +0900

    Update repost.blade.php

--- resources/views/review/repost.blade.php
+++ resources/views/review/repost.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,21 +9,28 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <p>レビュー確認画面</p>
     <form action="{{route('update')}}" method="post">
         @csrf
         <h1>確認内容</h1>
-        <input type="hidden" name="id" value={{$review->id}}>
+        <input type="hidden" name="id" value={{$record->id}}>
         <br>
-        <p>書籍名</p>
+        <p>書籍名{{ $review->book->title }}</p>
         <br>
-        レビュ－内容[{{$review->content}}]
+        レビュー内容:
+        <input type="text" name="content" value="{{ old('content', $review->content) }}">
         <br>
-        評価[{{$review->rating}}]
+        評価:
+        <input type="text" name="rating" value="{{ old('rating', $review->rating) }}">
+        <!-- レビュ－内容[{{$record->content}}]
+        <br>
+        評価[{{$record->rating}}] -->
         <br>
         <input type="submit" value="登録" class="btn btn-primary">
     </form>
     <a href="{{route('top')}}" class="btn btn-secondary">戻る</a>
 </body>
+
 </html>
