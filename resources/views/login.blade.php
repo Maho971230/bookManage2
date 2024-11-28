536e1dccf55f2534ea391e2021ffd35e8d9f261a
joe1883 <ks.joe1883@gmail.com>
2024-11-28 20:56:36 +0900

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -12,8 +12,8 @@
     <h1>社員名とパスワードを入力してください</h1>
     <form action="top" method="post">
         @csrf
-        社員名　　<input type="text" name="name" required><br>
-        パスワード <input type="password" name="password" id="password" required>
+        社員名　　<input type="text" name="name" class="form-control @error('name') is-invalid @enderror" required><br>
+        パスワード <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror" required>
         <input type="submit" value="ログイン">
         @error('name')
             <p style="color:red;">{{ $message }}</p>
