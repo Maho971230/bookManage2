ced6a88ef4008f2e2e1fb2c0c164e28f67c5addf
Kozasa2024 <kozasa14741@gmail.com>
2024-11-28 15:18:49 +0900

    Update postconf.blade.php

--- resources/views/review/postconf.blade.php
+++ resources/views/review/postconf.blade.php
@@ -4,18 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>新規レビュー確認画面</title>
+    <title>新規レビュー確認</title>
+    {{-- bootstrapの読み込み --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <p>新規レビュー確認画面</p>
-    <form action="" method="">
+    <h1>以下の内容で登録してもよろしいですか</h1>
+    <form action="/store" method="post">
         @csrf
-        <h1>確認内容</h1>
-        書籍名{{}}
-        著書名{{}}
-        レビュー内容{{}}
-    </form>
-    <input type="submit" value="登録">
-    <a href="">戻る</a>
+            <h1>確認内容</h1>
+            <input type="hidden" name="id" value="{{}}">
+            <br>
+            <p>書籍名</p> <br>
+            <p>著書名</p> <br>
+            評価[{{}}] <br>
+            レビュー内容[{{}}]
+            <br>
+            <input type="submit" value="登録">
+        </form>
+        <a href="{{route('top')}}" class="btn btn-secondary">戻る</a>
 </body>
 </html>
