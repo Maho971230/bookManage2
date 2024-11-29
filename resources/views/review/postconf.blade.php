ef08d1887ce2bb961c01ac2a8403a29573549f40
Kozasa2024 <kozasa14741@gmail.com>
2024-11-29 10:45:30 +0900

    Update postconf.blade.php

--- resources/views/review/postconf.blade.php
+++ resources/views/review/postconf.blade.php
@@ -10,18 +10,23 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
+    <p>レビュー新規登録</p>
     <h1>以下の内容で登録してもよろしいですか</h1>
     <form action="{{route('store')}}" method="post">
         @csrf
             <h1>確認内容</h1>
             <input type="hidden" name="id" value="{{$record->id}}">
             <br>
-            <p>書籍名</p> <br>
-            <p>著書名</p> <br>
-            評価[{{$record-rating}}] <br>
-            レビュー内容[{{$record->content}}]
+            <p>書籍名:{{ $review -> book -> title}}</p>
             <br>
-            <input type="submit" value="登録">
+            レビュー内容:
+            <p>{{ old('content', $review->content) }}</p>
+            <br>
+            評価:
+            <p>{{ old('rating', $review->rating) }}</p>            
+            <br>
+            
+            <input type="submit" value="登録" class="btn-primary">
         </form>
         <a href="{{route('top')}}" class="btn btn-secondary">戻る</a>
 </body>
