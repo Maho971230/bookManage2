9ed3ccbec38719a7219578f7a1a5deb8fb5adeb2
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-29 13:42:00 +0900

    Update searchList.blade.php

--- resources/views/search/searchList.blade.php
+++ resources/views/search/searchList.blade.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>検索結果</title>
+    <!-- CSSファイルへのリンク -->
+    <link rel="stylesheet" href="/assets/css/searchList.css">
     {{-- BootStrapから引用 --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
@@ -15,36 +17,43 @@
 </head>
 
 <body>
-    <a href="/top">Topページに戻る</a>
-    <h1>検索結果</h1>
+    <div class="logout-container">
+        <a href="/top" class="btn btn-success">Topページに戻る</a>
+    </div>
+    <div class="appTitle">
+        <h1>書籍管理システム</h1>
+    </div>
+    <div class="subTitle">
+        <h2>検索結果</h2>
+    </div>
     @if($results->isEmpty())
         <p>該当する書籍はありません。</p>
     @else
-
-            @csrf
-            <table class="table">
-                <thead>
-                    {{-- 目次 --}}
-                    <tr>
-                        <th scope="col">番号</th>
-                        <th scope="col">書籍名</th>
-                        <th scope="col">著書名</th>
-                        <th scope="col">詳細</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($results as $book)
-                        {{-- 中身 --}}
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>{{$book->title}}</td>
-                            <td>{{$book->writer}}</td>
-                            <td><a href="{{route('book',$book->id)}}" class="btn btn-primary">詳細</a></td>
+        @csrf
+            <div class="table">
+                <table class="table table-bordered border-success">
+                    <thead>
+                        {{-- 目次 --}}
+                        <tr class="table-active">
+                            <th scope="col">番号</th>
+                            <th scope="col">書籍名</th>
+                            <th scope="col">著書名</th>
+                            <th scope="col">詳細</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
+                    </thead>
+                    <tbody>
+                        @foreach ($results as $book)
+                            {{-- 中身 --}}
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>{{$book->title}}</td>
+                                <td>{{$book->writer}}</td>
+                                <td><a href="{{route('book',$book->id)}}" class="btn btn-outline-success">詳細</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
     @endif
 </body>
 </html>
