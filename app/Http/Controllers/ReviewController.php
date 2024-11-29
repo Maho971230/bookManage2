f72188835b2e75a51c8e8bcbcd1a1e2cd4d32626
Kozasa2024 <kozasa14741@gmail.com>
2024-11-29 11:00:17 +0900

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -20,6 +20,12 @@ class ReviewController extends Controller
     //新規登録確認画面
     public function postconf(Request $req)
     {
+        $id=$req=>input('id');
+        $record = Review::find($id);//新規登録のid値に該当するレコードを取得
+        if(!$record){
+            adort(404,'レビューが見つかりません。');
+        }
+
         return view('review.postconf');
     }
 
