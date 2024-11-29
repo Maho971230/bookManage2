27379d21c550e250a592c39dc04bf28efc51dad0
Maho971230 <2017sccmahoadachi@gmail.com>
2024-11-29 16:51:35 +0900

    Update searchWriter.blade.php

--- resources/views/search/searchWriter.blade.php
+++ resources/views/search/searchWriter.blade.php
@@ -4,13 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>著者名検索</title>
+    <!-- CSSファイルへのリンク -->
+    <link rel="stylesheet" href="/assets/css/searchWriter.css">
+    {{-- BootStrapから引用 --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <h1>著者名(キーワード)を入力してください</h1>
-    <form action="/searchList" method="post"><a href="/top">トップページに戻る</a>
-        @csrf
-        <input type="hidden" name="type" value="writer">
-        <input type="text" name="word" required><input type="submit" value="検索">
-    </form>
+    <div class="logout-container">
+        <a href="/top" class="btn btn-success">Topページに戻る</a>
+    </div>
+    <div class="appTitle">
+        <h1>書籍管理システム</h1>
+    </div>
+    <div class="subTitle">
+        <h2>著者名検索</h2>
+    </div>
+    <br>
+    著者名(キーワード)を入力してください
+    <div class="keywordWriter">
+        <form action="/searchList" method="post">
+            @csrf
+            <input type="hidden" name="type" value="writer" class="form-label border-success">
+            <input type="text" name="word" required><input type="submit" value="検索" class="btn btn-outline-success">
+        </form>
+    </div>
 </body>
 </html>
