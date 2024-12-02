01b97db66828a5685fe6d01ed614e5b4c1ae7e71
Maho971230 <2017sccmahoadachi@gmail.com>
2024-12-02 10:01:33 +0900

    Update edit.blade.php

--- resources/views/review/edit.blade.php
+++ resources/views/review/edit.blade.php
@@ -11,31 +11,20 @@
 </head>
 
 <body>
-    <form action="/repost" method="post"> <!-- 修正先のルートに対応 -->
-        @csrf
-        @if(isset($record))
-        <input type="hidden" name="id" value="{{ $record->id }}">
-        <br>
-        <p>書籍名:{{ $review->book->title }}</p>
-        <br>
-        レビュー内容<textarea name="content">{{ $record->content }}</textarea>
-        <br>
-        評価
-        {{-- <input type="text" name="rating" value="{{ $record->rating }}"> --}}
-        <div class="star-rating">
-            @php
-                $rating = old('rating', $review->rating ?? 1);
-            @endphp
-            @for ($i = 1; $i <= 5; $i++)
-                <i class="fa fa-star {{ $rating >= $i ? 'text-warning' : 'text-muted' }}"></i>
-            @endfor
-        </div>  
-        <br>
-        <input type="submit" value="修正" class="btn btn-primary">
-        @else
-        <p>レビュー情報が見つかりません。</p>
-        @endif
-    </form>
+<form action="/repost" method="post"> <!-- 修正先のルートに対応 -->
+    @csrf
+    @if(isset($record))
+    <input type="hidden" name="id" value="{{ $record->id }}">
+    <br>
+    レビュー内容<textarea name="content">{{ $record->content }}</textarea>
+    <br>
+    評価<input type="text" name="rating" value="{{ $record->rating }}">
+    <br>
+    <input type="submit" value="修正" class="btn btn-primary">
+    @else
+    <p>レビュー情報が見つかりません。</p>
+    @endif
+			</form>
     <a href="{{route('top')}}" class="btn btn-secondary">戻る</a>
 </body>
 
